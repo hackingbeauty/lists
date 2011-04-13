ea15f2cae4f2ee361d81f8522d7c6e7bcff70536
Mark Muskardin <markmuskardin@gmail.com>
2011-04-12 23:03:18 -0700

    Added basic url functionality

--- app/controllers/url_controller.rb
+++ app/controllers/url_controller.rb
@@ -1,15 +1,17 @@
 class UrlController < ApplicationController
-
+  skip_before_filter :verify_authenticity_token
+  
   def create
-    url = request.raw_data
-    puts "======"
-    puts "url is #{url}"
+    post_data = request.body.read
+    url = Url.find_or_create_by_name(post_data)
+    url.save
   end
   
   def new
   end
   
   def index
+    @urls = Url.find(:all)
   end
   
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Lists::Application.routes.draw do
   resources :url
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-
+  match 'submit_url' => 'url#create'
+  
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
@@ -49,7 +51,7 @@ Lists::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "url#index"
 
   # See how all your routes lay out with "rake routes"
 
