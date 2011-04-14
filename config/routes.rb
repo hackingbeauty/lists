fa492b47bf85f97a9a45dd402af4d12e23dadef3
Mark Muskardin <markmuskardin@gmail.com>
2011-04-13 22:41:26 -0700

    chrome extension working! extension successfully sending tab's current url

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Lists::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-  match 'submit_url' => 'url#create'
+  match 'submit_url' => 'urls#create'
   
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
