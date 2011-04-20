61489d6491897f1d8944a8b887ab39a49f6bafb1
Mark Muskardin <markmuskardin@gmail.com>
2011-04-19 22:36:09 -0700

    added mootools with drag-and-drop functionality and initial stylesheets

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,46 @@
 (function(){
   
-  Timer = {
-    init: function(){
-      
+  List = {
+    dragDrop: function(){
+      $$('.item').addEvent('mousedown', function(event){
+         event.stop();
+
+         // `this` refers to the element with the .item class
+          var shirt = this;
+
+         var clone = shirt.clone().setStyles(shirt.getCoordinates()).setStyles({
+           opacity: 0.7,
+           position: 'absolute'
+         }).inject(document.body);
+
+         var drag = new Drag.Move(clone, {
+
+           droppables: $('brain'),
+
+           onDrop: function(dragging, cart){
+
+             dragging.destroy();
+
+             if (cart != null){
+               shirt.clone().inject(cart);
+               cart.highlight('#7389AE', '#FFF');
+             }
+           },
+           onEnter: function(dragging, cart){
+             cart.tween('background-color', '#98B5C1');
+           },
+           onLeave: function(dragging, cart){
+             cart.tween('background-color', '#FFF');
+           },
+           onCancel: function(dragging){
+             dragging.destroy();
+           }
+         });
+         drag.start(event);
+       });
     }
   }
   
-  Timer.init();
+  List.dragDrop();
   
 })();
