33ab67802987a71a7c2ceb804011e0ff79056b0f
Mark Muskardin <markmuskardin@gmail.com>
2011-04-20 12:48:38 -0700

    Removed mootools, added jquery 1.5.2-min

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,34 +6,7 @@
       List.dragDrop();
     },
     dragDrop: function(){
-      $$('.neuron').addEvent('mousedown', function(event){
-         event.stop();
-         var item = this; // `this` refers to the element with the .item class
-         var clone = item.clone().setStyles(item.getCoordinates()).setStyles({
-           opacity: 0.7,
-           position: 'absolute'
-         }).inject(document.body);
-         var drag = new Drag.Move(clone, {
-           droppables: $('hippocampus'),
-           onDrop: function(dragging, hippocampus){
-             dragging.destroy();
-             if (hippocampus != null){
-               item.clone().inject(hippocampus);
-               hippocampus.highlight('#1ef80e', '#FFF');
-             }
-           },
-           onEnter: function(dragging, hippocampus){
-             hippocampus.tween('background-color', '#1ef80e');
-           },
-           onLeave: function(dragging, hippocampus){
-             hippocampus.tween('background-color', '#FFF');
-           },
-           onCancel: function(dragging){
-             dragging.destroy();
-           }
-         });
-         drag.start(event);
-       });
+
     },
     neuronSelection:function(){
       // $('.neuron').click(function() {
