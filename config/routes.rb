baa32bf4d8e80cf79de33ebd871ddd481e065a4e
Mark Muskardin <markmuskardin@gmail.com>
2011-04-30 15:33:27 -0700

    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Lists::Application.routes.draw do
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
   match 'submit_url' => 'urls#create'
-  
+
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
