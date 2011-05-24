9d23271ffe9de2d210ab3fd4fc16ec031e145453
Mark Muskardin <markmuskardin@gmail.com>
2011-05-24 13:18:21 -0700

    added dragleave, dragover, and dragenter events

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,31 +1,55 @@
 (function(){
   
   BrainList = {
-    dragOverDiv: $('#hippocampus'),
     init:function(){
-      BrainList.dragDrop();
-    },
-    dragDrop: function(){
       if (Modernizr.draganddrop) {
-        $('#list').delegate('a', 'dragstart',  function(){ $(this).toggleClass('opaque')   });
-        $('#list').delegate('a', 'dragenter',  function(event){ BrainList.dragEnter(event) });
-        $('#hippocampus').bind('dragover', function(eventObj){ BrainList.dragOver(event)  });
-        $('#list').delegate('a', 'dragleave',  function(event){ BrainList.dragOver(event)  });
+        $('#list').delegate('a', 'dragstart',  function(){ BrainList.handleDragStart(this) });
+        var dragOverArea = $('#hippocampus');
+        $(dragOverArea).bind('dragover',  function(){ BrainList.handleDragOver(this) });
+        $(dragOverArea).bind('dragleave',  function(){ BrainList.handleDragLeave(this) });    
       } else {
         // Fallback to a library solution.
       }
     },
-    dragEnter: function(event){
-      $(BrainList.dragOverDiv).toggleClass('opaque');
+    handleDragStart:function(elem){
+      $(elem).addClass('opaque');
     },
-    dragOver: function(eventObj){
-
+    handleDragOver:function(elem){
+      $(elem).addClass('over');
     },
-    dragLeave: function(event){
-      $(BrainList.dragOverDiv).toggleClass('opaque');
-      console.log("dragLeave is " + event);
+    handleDragLeave:function(elem){
+      $(elem).removeClass('over');
     }
   }
+  
+
+  
+  // BrainList = {
+  //   dragOverDiv: $('#hippocampus'),
+  //   init:function(){
+  //     BrainList.dragDrop();
+  //   },
+  //   dragDrop: function(){
+  //     if (Modernizr.draganddrop) {
+  //       $('#list').delegate('a', 'dragstart',  function(){ $(this).toggleClass('opaque')   });
+  //       $('#list').delegate('a', 'dragenter',  function(event){ BrainList.dragEnter(event) });
+  //       $('#hippocampus').bind('dragover', function(eventObj){ BrainList.dragOver(event)  });
+  //       $('#list').delegate('a', 'dragleave',  function(event){ BrainList.dragOver(event)  });
+  //     } else {
+  //       // Fallback to a library solution.
+  //     }
+  //   },
+  //   dragEnter: function(event){
+  //     $(BrainList.dragOverDiv).toggleClass('opaque');
+  //   },
+  //   dragOver: function(eventObj){
+  //     console.log("dragover");
+  //   },
+  //   dragLeave: function(event){
+  //     $(BrainList.dragOverDiv).removeClass('opaque');
+  //     console.log("dragLeave is " + event);
+  //   }
+  // }
     
   BrainList.init();
   
