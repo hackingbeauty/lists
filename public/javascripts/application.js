e072311a05115b8a0e5dee793f298fb260cdef40
Mark Muskardin <markmuskardin@gmail.com>
2011-06-15 12:55:15 -0700

    cleaned up application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -60,34 +60,7 @@
       });
     }
   }
-  
-  // BrainList = {
-  //   dragOverDiv: $('#hippocampus'),
-  //   init:function(){
-  //     BrainList.dragDrop();
-  //   },
-  //   dragDrop: function(){
-  //     if (Modernizr.draganddrop) {
-  //       $('#list').delegate('a', 'dragstart',  function(){ $(this).toggleClass('opaque')   });
-  //       $('#list').delegate('a', 'dragenter',  function(event){ BrainList.dragEnter(event) });
-  //       $('#hippocampus').bind('dragover', function(eventObj){ BrainList.dragOver(event)  });
-  //       $('#list').delegate('a', 'dragleave',  function(event){ BrainList.dragOver(event)  });
-  //     } else {
-  //       // Fallback to a library solution.
-  //     }
-  //   },
-  //   dragEnter: function(event){
-  //     $(BrainList.dragOverDiv).toggleClass('opaque');
-  //   },
-  //   dragOver: function(eventObj){
-  //     console.log("dragover");
-  //   },
-  //   dragLeave: function(event){
-  //     $(BrainList.dragOverDiv).removeClass('opaque');
-  //     console.log("dragLeave is " + event);
-  //   }
-  // }
-    
+      
   BrainList.init();
   
 })();
