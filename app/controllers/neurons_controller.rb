8859e326ef7504b201a90b0e25a4ac418f8e84ad
Mark Muskardin <markmuskardin@gmail.com>
2011-06-16 16:38:53 -0700

    Evernote authentication successful\!

--- app/controllers/neurons_controller.rb
+++ app/controllers/neurons_controller.rb
@@ -1,19 +1,35 @@
+require 'rubygems'
 require 'evernote'
+require 'pp'
+require 'open-uri'
 
 class NeuronsController < ApplicationController
   
   def create
-    @neuron = Neuron.new
-    @neuron.url = params[:data]
-    respond_to do |type|
-      if @neuron.save
-        url = Url.find_or_create_by_name(@neuron.url)
-        url.delete #delete the url after you've created a neuron
-        type.js { render :text => "successfully registered neuron" }
-      else
-        type.js { render :text => "Error saving neuron", :status => :unprocessable_entity } 
-      end
-    end
+    user_store_url = "https://sandbox.evernote.com/edam/user"
+    unprocessable_entityser_store_url = "https://sandbox.evernote.com/edam/user"
+    config = ::Rails.root.to_s + "/config/evernote.yml"
+    user_store = Evernote::UserStore.new(user_store_url,config,'sandbox')
+    auth_result = user_store.authenticate
+    user = auth_result.user
+    
+    puts "user is #{user.inspect}"
+
+#    @neuron = Neuron.new
+#    @neuron.url = params[:data]
+#    respond_to do |type|
+#      if @neuron.save
+#        url = Url.find_or_create_by_name(@neuron.url)
+#        url.delete #delete the url after you've created a neuron
+#        type.js { render :text => "successfully registered neuron" }
+#      else
+#        type.js { render :text => "Error saving neuron", :status => :unprocessable_entity } 
+#      end
+#    end
+    #
+  
+    render :nothing => true
+    
   end
   
   def index
