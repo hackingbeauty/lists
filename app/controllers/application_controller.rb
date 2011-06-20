3232d0415906b65a08b96a482434bc4092807df2
Mark Muskardin <markmuskardin@gmail.com>
2011-06-19 20:38:15 -0700

    cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-#oequire "config/evernote_config.rb"
+require "config/evernote_config.rb"
 
 class ApplicationController < ActionController::Base
   protect_from_forgery
@@ -8,28 +8,20 @@ class ApplicationController < ActionController::Base
     userStoreTransport = Thrift::HTTPClientTransport.new(userStoreUrl)
     userStoreProtocol = Thrift::BinaryProtocol.new(userStoreTransport)
     userStore = Evernote::EDAM::UserStore::UserStore::Client.new(userStoreProtocol)    
-    config = ::Rails.root.to_s + "/config/evernote.yml"
-    auth_result = userStore.authenticate(config)
-    @user = auth_result.user
-    puts "user is #{@user.name}"
-
-    puts "successfully loaded evernote libs i guess" 
-
-    #noteStoreUrl = "https://sandbox.evernote.com/edam/note/" + shard_id
-    #noteStoreTransport = Thrift::HTTPClientTransport.new(noteStoreUrl)
-    #noteStoreProtocol = Thrift::BinaryProtocol.new(noteStoreTransport)
-    #noteStore = Evernote::EDAM::NoteStore::NoteStore::Client.new(noteStoreProtocol)
-        
     
-#    user_store_url = "https://sandbox.evernote.com/edam/user"
-#    unprocessable_entityser_store_url = "https://sandbox.evernote.com/edam/user"
-#    config = ::Rails.root.to_s + "/config/evernote.yml"
-#    @user_store = Evernote::UserStore.new(user_store_url,config,'sandbox')
-#    auth_result = @user_store.authenticate
-#    outh_token = auth_result.authenticationToken
-#    @user = auth_result.user
-#    note_store_url = "http://sandbox.evernote.com/edam/note/#{@user.shardId}"
-#    @note_store = Evernote::NoteStore.new(note_store_url)
+    @config = YAML::load(File.open("#{::Rails.root.to_s}/config/evernote.yml"))
+
+    authResult = userStore.authenticate(@config['sandbox']['username'],
+                                         @config['sandbox']['password'],
+                                         @config['sandbox']['consumer_key'],
+                                         @config['sandbox']['consumer_secret'])
+    @user = authResult.user
+    @authToken = authResult.authenticationToken
+
+    noteStoreUrl = "https://sandbox.evernote.com/edam/note/" + @user.shardId
+    noteStoreTransport = Thrift::HTTPClientTransport.new(noteStoreUrl)
+    noteStoreProtocol = Thrift::BinaryProtocol.new(noteStoreTransport)
+    @noteStore = Evernote::EDAM::NoteStore::NoteStore::Client.new(noteStoreProtocol)
   end
 
 end
--- app/controllers/neurons_controller.rb
+++ app/controllers/neurons_controller.rb
@@ -6,20 +6,18 @@ class NeuronsController < ApplicationController
     neuron = params[:data] 
     
     begin
-      #note = Evernote::EDAM::Type::Note.new()
-      #note = Evernote::NoteStore::Type::Note.new
-      
-      #note.title = neuron
-      #note.content = neuron
-      #@note_store.createNote(@auth_token,note)
+      note = Evernote::EDAM::Type::Note.new()
+      note.title = neuron
+      note.content = neuron
+      @noteStore.createNote(@authToken,note)
       puts "neuron successfully stored"
-    rescue Exception => e
-      puts "Evernote error: #{e.message}"
+    rescue Evernote::EDAM::Error::EDAMErrorCode => e
+     puts "Evernote error: #{e.message}"
     end
 
     render :nothing => true
   end
-  
+
   def index
     @neurons = Neuron.find(:all)
   end
